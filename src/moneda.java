6c91395e47e6036527cf4658ab5016850efaac63
carlos <caralfmo@gmail.com>
2023-07-01 09:25:22 -0500

    Prueba de moneda

--- src/moneda.java
+++ src/moneda.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class moneda {
+import java.util.HashMap;
+import java.util.Map;
+
+public class moneda {
+    private String nombre;
+    private Map<String, Double> valores;
+
+    public moneda() {
+        nombre = "Peso Colombiano";
+        valores = new HashMap<>();
+
+        valores.put("Dolar", 0.00024);
+    }
+
+    public moneda(String nombre, Map valores) {
+        this.nombre = nombre;
+        this.valores = valores;
+    }
+
+    public Map<String, Double> getValores() {
+        return valores;
+    }
+
+    public void setValores(Map<String, Double> valores) {
+        this.valores = valores;
+    }
+
+    public String conversion(int cantidad, String moneda){
+        Double a = cantidad * this.valores.get(moneda);
+        return "hola " + this.valores.get(moneda);
+    }
 }
