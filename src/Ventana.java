d3a09a1c7fd5d6f43214b09107bb1ce5eda542a5
carlos <caralfmo@gmail.com>
2023-07-01 11:18:39 -0500

    Diseño de ventana principal

--- src/Ventana.java
+++ src/Ventana.java
@@ -8,24 +8,32 @@ public class Ventana extends JFrame{
         iniciarComponentes();
 
         setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     private void iniciarComponentes(){
         JPanel panel = new JPanel();
 
+        panel.setLayout(null);
         this.getContentPane().add(panel);
 
-        JLabel texto = new JLabel("Pesos Colombianos");
+        JLabel texto = new JLabel();
+        texto.setText("Pesos Colombianos");
+        texto.setBounds(50, 5, 200, 20);
         panel.add(texto);
 
         JTextField cantidad = new JTextField();
+        cantidad.setBounds(50, 50, 200, 20);
         panel.add(cantidad);
 
         String monedas[] = {"Dolar", "Euro", "Libra", "Yen", "Won"};
         JList moneda = new JList<>(monedas);
+        moneda.setBounds(50, 100, 200, 20);
         panel.add(moneda);
 
         JButton convertir = new JButton("Convertir");
+        convertir.setBounds(50, 150, 100, 20);
         panel.add(convertir);
     }
 }
